c6383644ee4e32b2529c9dd20f713e0a1ffd9ff2
zachcannon <zach.cannon10@gmail.com>
2014-06-15 13:18:27 -0500

    Added new route for home page

--- AgileTaskKeeper/App_Start/WebApiConfig.cs
+++ AgileTaskKeeper/App_Start/WebApiConfig.cs
@@ -19,6 +19,12 @@ namespace AgileTaskKeeper
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "HomeApi",
+                routeTemplate: "Home/api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
