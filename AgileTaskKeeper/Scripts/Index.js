21284c120f603fb79c63c6c41fcc9a92109725e1
Zachary Cannon <zach.cannon10@gmail.com>
2014-06-29 17:03:38 -0500

    Added angularjs, refactored code to impliment angular.

--- AgileTaskKeeper/Scripts/Index.js
+++ AgileTaskKeeper/Scripts/Index.js
@@ -1,55 +1,55 @@
-﻿$(function () {
-    $('.collapse').collapse();
+﻿var app = angular.module("agileIndexApp", []);
 
-    displayAllTasks();
+app.controller("agileIndexController", function ($scope, $http) {
+    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    $http.defaults.headers.put["Content-Type"] = "application/x-www-form-urlencoded";
 
-    $("#saveTask").click(function () {
-        $.post('/api/AgileTask',
-            $('#newTaskForm').serialize(),
-            null,
-            'json').done(function (data) {
-                noty({ 'text': 'Task Successfully Added!', 'timeout': '5000' });
-                $('#newTaskForm').trigger("reset");
-                displayAllTasks();
-            })
-
-    });
-
-    $("#updateTask").click(function () {
-        var disabled = $('#updateTaskForm').find(':input:disabled').removeAttr('disabled');
-        $.ajax({
-            url: '/api/AgileTask',
-            type: 'PUT',
-            data: $('#updateTaskForm').serialize(),
-            success: function (data) {
-                if (data == 'pass') {
-                    noty({ 'text': 'Task Successfully Updated!', 'timeout': '5000' });
-                    $('#updateTaskForm').trigger("reset");
-                    displayAllTasks();
-                } else {
-                    noty({ 'text': 'The task was not updated, an error occured...', 'timeout': '5000' });
-                    $('#updateTaskForm').trigger("reset");
-                }
-            }
-        });
-        disabled.attr('disabled', 'disabled');
-    });
-
-    function formatItem(task) {
-        return "Title: <span>" + task.Title + "</span> <br/>Body: " + task.Body;
-    };
-
-    function displayAllTasks() {
-        $('#allTasks').empty();
-        $.getJSON('/api/AgileTask')
-            .done(function (data) {
-                $.each(data, function (key, item) {
-                    $('#allTasks').append("<li class='taskListItem' id='item" + key + "'>" + formatItem(item) + "</li>")
-                });
-                $("#allTasks li").click(function () {
-                    $('#updateTitle').empty();
-                    $('#updateTitle').val($('#' + this.id + ' span').text());
-                });
+    $scope.displayAllTasks = function () {
+        $http.get('/api/AgileTask').
+            success(function (data) {
+                $scope.listOfTasks = data;
+            }).
+            error(function () {
+                noty({ 'text': 'Error retrieving tasks from backend...', 'timeout': '5000' });
             });
     };
+
+    $scope.saveTask = function () {
+        $http.post('/api/AgileTask', $('#newTaskForm').serialize()).
+            success(function (data) {
+                noty({ 'text': 'Task Successfully Added!', 'timeout': '5000' });
+                $scope.newFormBody = "";
+                $scope.newFormTitle = "";
+                $scope.displayAllTasks();
+            }).
+            error(function () {
+                noty({ 'text': 'Error saving task...', 'timeout': '5000' });
+            });
+    };
+
+    $scope.updateTask = function () {
+        var disabled = $('#updateTaskForm').find(':input:disabled').removeAttr('disabled');
+        $http.put('/api/AgileTask', $('#updateTaskForm').serialize()).
+            success(function (data) {
+                if (data == 'pass') {
+                    noty({ 'text': 'Task Successfully Updated!', 'timeout': '5000' });
+                    $scope.updateFormTitle = "";
+                    $scope.updateFormBody = "";
+                    $scope.displayAllTasks();
+                } else {
+                    noty({ 'text': 'The task was not updated, an error occured...', 'timeout': '5000' });
+                }
+            });
+
+        disabled.attr('disabled', 'disabled');
+    };
+
+    $scope.updateTitleBox = function (task) {
+        $scope.updateFormTitle = task.Title;
+    }
+
+
+    $('.collapse').collapse();
+
+    $scope.displayAllTasks();
 });
